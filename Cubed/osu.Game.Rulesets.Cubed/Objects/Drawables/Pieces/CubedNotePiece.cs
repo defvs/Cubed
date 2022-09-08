105eb579fec42fd54828f2774b50ef01e2de40ea
Alban Cabannes-Michel <alban.cello2@gmail.com>
2022-09-09 01:23:08 +0200

    Significant slim-down of CubedNotePiece.cs
    Yes I tested my changes

--- Cubed/osu.Game.Rulesets.Cubed/Objects/Drawables/Pieces/CubedNotePiece.cs
+++ Cubed/osu.Game.Rulesets.Cubed/Objects/Drawables/Pieces/CubedNotePiece.cs
@@ -1,22 +1,10 @@
 ﻿using osu.Framework.Graphics;
 using osu.Framework.Graphics.Shapes;
-using osuTK;
 
 namespace osu.Game.Rulesets.Cubed.Objects.Drawables.Pieces
 {
     public class CubedNotePiece : Box
     {
-        public CubedNotePiece()
-        {
-            RelativeSizeAxes = Axes.Both;
-            RelativePositionAxes = Axes.Both;
-            Anchor = Anchor.Centre;
-            Origin = Anchor.Centre;
-            Size = Vector2.One;
-            Alpha = 1;
-            Position = Vector2.Zero;
-            Colour = Colour4.White;
-            Invalidate();
-        }
+        public CubedNotePiece() { RelativeSizeAxes = Axes.Both; }
     }
 }
