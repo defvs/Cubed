cb2af35418caee35c38e0fc045d2e19363dad2cd
Alban Cabannes-Michel <alban.cello2@gmail.com>
2022-09-11 17:38:47 +0200

    Implement touchscreen input

--- Cubed/osu.Game.Rulesets.Cubed/CubedInputManager.cs
+++ Cubed/osu.Game.Rulesets.Cubed/CubedInputManager.cs
@@ -2,11 +2,13 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.ComponentModel;
+using osu.Framework.Allocation;
 using osu.Framework.Input.Bindings;
 using osu.Game.Rulesets.UI;
 
 namespace osu.Game.Rulesets.Cubed
 {
+    [Cached]
     public class CubedInputManager : RulesetInputManager<CubedAction>
     {
         public CubedInputManager(RulesetInfo ruleset)
