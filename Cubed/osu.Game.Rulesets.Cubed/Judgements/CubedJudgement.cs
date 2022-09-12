5b3e621956fa82b419da2af26e06b77ba0f316a9
Alban Cabannes-Michel <alban.cello2@gmail.com>
2022-09-12 14:50:17 +0200

    Revert CubedJugement's MaxResult to the default behavior

--- Cubed/osu.Game.Rulesets.Cubed/Judgements/CubedJudgement.cs
+++ Cubed/osu.Game.Rulesets.Cubed/Judgements/CubedJudgement.cs
@@ -1,10 +1,6 @@
 using osu.Game.Rulesets.Judgements;
-using osu.Game.Rulesets.Scoring;
 
 namespace osu.Game.Rulesets.Cubed.Judgements
 {
-    public class CubedJudgement : Judgement
-    {
-        public override HitResult MaxResult => HitResult.Great;
-    }
+    public class CubedJudgement : Judgement { }
 }
