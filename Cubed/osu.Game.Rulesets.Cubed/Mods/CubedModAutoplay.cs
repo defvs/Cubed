45be65d3faa9bb1aa76aae2f2fe489d84908b6d4
Alban Cabannes-Michel <alban.cello2@gmail.com>
2022-09-27 00:33:40 +0200

    Add an imperfection setting to autoplay

--- Cubed/osu.Game.Rulesets.Cubed/Mods/CubedModAutoplay.cs
+++ Cubed/osu.Game.Rulesets.Cubed/Mods/CubedModAutoplay.cs
@@ -2,7 +2,9 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
+using osu.Framework.Bindables;
 using osu.Game.Beatmaps;
+using osu.Game.Configuration;
 using osu.Game.Rulesets.Cubed.Replays;
 using osu.Game.Rulesets.Mods;
 
@@ -10,9 +12,16 @@ namespace osu.Game.Rulesets.Cubed.Mods
 {
     public class CubedModAutoplay : ModAutoplay
     {
-        public override ModReplayData CreateReplayData(IBeatmap beatmap, IReadOnlyList<Mod> mods)
+        [SettingSource("Error margin", "Maximum autoplay error margin")]
+        public BindableNumber<double> ArtificialErrorMargin { get; } = new BindableDouble(0)
         {
-            return new ModReplayData(new CubedAutoGenerator(beatmap).Generate(), new ModCreatedUser { Username = "Concierge" });
-        }
+            MinValue = 0,
+            MaxValue = 168,  // TODO fetch the value because it could be changed by stuff like double time
+            Precision = 1
+        };
+
+    public override ModReplayData CreateReplayData(IBeatmap beatmap, IReadOnlyList<Mod> mods)
+             => new ModReplayData(new CubedAutoGenerator(beatmap, ArtificialErrorMargin.Value).Generate(),
+                                  new ModCreatedUser { Username = "Concierge" });
     }
 }
