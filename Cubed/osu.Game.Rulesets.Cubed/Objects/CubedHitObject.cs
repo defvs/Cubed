0667ea7b28ffb811975297aaa3467775b4dd149d
Alban Cabannes-Michel <alban.cello2@gmail.com>
2022-10-13 04:04:20 +0200

    CubedHitObject's Positions are now Binables
    This change is for the editor so now hitobjects can move

--- Cubed/osu.Game.Rulesets.Cubed/Objects/CubedHitObject.cs
+++ Cubed/osu.Game.Rulesets.Cubed/Objects/CubedHitObject.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Bindables;
 using osu.Game.Rulesets.Cubed.Judgements;
 using osu.Game.Rulesets.Cubed.Scoring;
 using osu.Game.Rulesets.Judgements;
@@ -14,26 +15,32 @@ namespace osu.Game.Rulesets.Cubed.Objects
     public class CubedHitObject : HitObject
     {
         /**
-         * X position ranges from 0 to 3, starting from 0 on the left and increasing rightwards
+         * X position ranges from 0 to 3, starting from 0 from the left and increasing rightwards
          */
-        public readonly byte PositionX;
+        public BindableNumber<byte> PositionX { get; } = new BindableNumber<byte>()
+        {
+            MaxValue = 3
+        };
 
         /**
          * Y position ranges from 0 to 3, 0 is the top and it increases downwards
          */
-        public readonly byte PositionY;
+        public BindableNumber<byte> PositionY { get; } = new BindableNumber<byte>()
+        {
+            MaxValue = 3
+        };
 
         public readonly CubedAction action;
 
         public CubedHitObject(byte positionX, byte positionY)
         {
-            PositionX = Math.Clamp(positionX, (byte)0, (byte)3);
-            PositionY = Math.Clamp(positionY, (byte)0, (byte)3);
+            PositionX.Value = Math.Clamp(positionX, (byte)0, (byte)3);
+            PositionY.Value = Math.Clamp(positionY, (byte)0, (byte)3);
 
             action = (CubedAction)(positionX + positionY * 4);
         }
 
-        public Vector2 PositionRelative => new Vector2(PositionX / 4f, PositionY / 4f);
+        public Vector2 PositionRelative => new Vector2(PositionX.Value / 4f, PositionY.Value / 4f);
 
         public override Judgement CreateJudgement() => new CubedJudgement();
 
